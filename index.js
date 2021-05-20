a5520d9442c92f0d726f63154cac4d72b03b1a0c
monglv <levanmong.dola.99@gmail.com>
2021-05-20 14:14:28 +0700

    Tăng dung lượng upload

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ const corsOptions = {
 };
 // app.use(cors(corsOptions));
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
